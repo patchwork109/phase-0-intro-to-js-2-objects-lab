738886596e909582e03a1229da040ee33349275c
Samantha Genevay <samanthagenevay@gmail.com>
2023-01-22 09:01:41 -0600

    commit tree beca8910124ea333a00e5003c8f5caa026607321

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+   const newEmployee = {...employee};
+   newEmployee[name] = "Tom";
+   newEmployee[streetAddress] = "12 Broadway";
+    return newEmployee;
+} 
